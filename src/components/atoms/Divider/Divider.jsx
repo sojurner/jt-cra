e7f4b73dadbbc56dccdaa4963629a0d59c5ecf08
Paul Kim <paul.kim@catalystrms.com>
2019-07-15 21:58:48 -0600

    Add divider atom

---
+++ src/components/atoms/Divider/Divider.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Divider = () => {
+  return (
+    <div
+      style={{
+        transform: 'rotate(90deg)',
+        borderTop: '1px solid black'
+      }}
+    />
+  );
+};
+
+export default Divider;
