24e8647a9dbb4ea1af5334f5fe56104d8b5a563b
Paul Kim <paul.kim@catalystrms.com>
2019-07-16 21:31:27 -0600

    Refactor styles

--- src/components/compounds/Profile/Profile.jsx
+++ src/components/compounds/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProfileTab from '../../molecules/ProfileTab';
 import styles from '../../../styles/components/compounds/Profile.module.scss';
 
 const Profile = ({ className }) => {
@@ -12,35 +13,21 @@ const Profile = ({ className }) => {
 
   return (
     <div className={styles[className]}>
-      <section
-        onClick={() => {
-          window
-            .open('https://www.mdpi.com/2076-3921/7/3/37/htm', '_blank')
-            .focus();
+      <div
+        style={{
+          height: 187,
+          width: '100%',
+          backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1do2fiqX2jxZ-cSy3EWEPzIF0TRn3ziAL_rABTonJ6UszAt9S')`,
+          backgroundRepeat: 'no-repeat',
+          position: 'absolute',
+          backgroundSize: 'cover',
+          zIndex: -1,
+          top: 0,
+          left: 0
         }}
-        className={styles.container}
-      >
-        <img
-          className={styles.img1}
-          src="https://media.licdn.com/dms/image/C5603AQHtBUQmDP1_6Q/profile-displayphoto-shrink_800_800/0?e=1568851200&v=beta&t=hnUWIEBo-kLVK7qBsNW1kd225zvJyf_SpcMilyZ84CY"
-          alt="john tan profile"
-        />
-        <div className={styles.section1}>
-          <div
-            className={
-              mounted ? styles['section1-1'] : styles['section1-1_hide']
-            }
-          >
-            <h3>Data Scientist | Clinical Research</h3>
-            <h4>Biostatistics</h4>
-          </div>
-          <img
-            className={styles.img2}
-            src="http://logodesignfx.com/wp-content/uploads/2019/04/mychart-logo-.png"
-            alt="loma linda emblem"
-          />
-        </div>
-      </section>
+      />
+      <ProfileTab mounted={mounted} styles={styles} />
+      {/* <Cards list={} /> */}
     </div>
   );
 };
---
+++ src/components/molecules/ProfileTab/ProfileTab.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ProfileTab = ({ mounted, styles }) => {
+  return (
+    <section
+      onClick={() => {
+        window
+          .open('https://www.mdpi.com/2076-3921/7/3/37/htm', '_blank')
+          .focus();
+      }}
+      className={styles.container}
+    >
+      <img
+        className={styles.img1}
+        src="https://media.licdn.com/dms/image/C5603AQHtBUQmDP1_6Q/profile-displayphoto-shrink_800_800/0?e=1568851200&v=beta&t=hnUWIEBo-kLVK7qBsNW1kd225zvJyf_SpcMilyZ84CY"
+        alt="john tan profile"
+      />
+      <div className={styles.section1}>
+        <div
+          className={mounted ? styles['section1-1'] : styles['section1-1_hide']}
+        >
+          <h3>Data Scientist | Clinical Research</h3>
+          <h4>Biostatistics</h4>
+        </div>
+        <img
+          className={styles.img2}
+          src="http://logodesignfx.com/wp-content/uploads/2019/04/mychart-logo-.png"
+          alt="loma linda emblem"
+        />
+      </div>
+    </section>
+  );
+};
+export default ProfileTab;
